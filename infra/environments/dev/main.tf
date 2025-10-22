d1e5aefc1bba332878d087668dcaea061f67897e
Dimple Kundu <kavitakundu04@gmail.com>
2025-10-22 12:08:04 +0530

    fixed private key

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -27,18 +27,3 @@ module "nomad_client" {
   nomad_server_private_ip = module.nomad_server.server_private_ip
 }
 
-# --- Outputs ---
-output "server_public_ip" {
-  description = "Nomad Server Public IP"
-  value       = module.nomad_server.server_public_ip
-}
-
-output "client_public_ips" {
-  description = "List of Nomad Client Public IPs"
-  value       = module.nomad_client.client_public_ips
-}
-
-output "nomad_ui_url" {
-  description = "Nomad UI Access URL"
-  value       = "http://${module.nomad_server.server_public_ip}:4646"
-}
--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -1,1 +1,15 @@
-# Outputs are defined in main.tf
+# Outputs 
+output "server_public_ip" {
+  description = "Nomad Server Public IP"
+  value       = module.nomad_server.server_public_ip
+}
+
+output "client_public_ips" {
+  description = "List of Nomad Client Public IPs"
+  value       = module.nomad_client.client_public_ips
+}
+
+output "nomad_ui_url" {
+  description = "Nomad UI Access URL"
+  value       = "http://${module.nomad_server.server_public_ip}:4646"
+}
